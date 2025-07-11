58e9ae8c8a892f152c81e7cea7a4aa32396f7926
VoVka <v228a@proton.me>
2025-07-11 20:07:22 +0300

    Mini changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
 
 export const metadata: Metadata = {
-  title: 'TypeAce',
+  title: 'TouchTyping',
   description: 'A clean, focused blind typing test app.',
 };
 
@@ -15,8 +15,6 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased">
